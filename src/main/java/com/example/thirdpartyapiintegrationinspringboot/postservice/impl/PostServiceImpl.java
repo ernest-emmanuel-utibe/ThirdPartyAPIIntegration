736ba1dfd1962c286077db7913d5ab6d45efccd6
Ernest Emmanuel Utibe <em123nest@gmail.com>
2024-02-09 12:00:50 +0100

    Update PostServiceImpl.java

--- src/main/java/com/example/thirdpartyapiintegrationinspringboot/postservice/impl/PostServiceImpl.java
+++ src/main/java/com/example/thirdpartyapiintegrationinspringboot/postservice/impl/PostServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
@@ -27,6 +28,7 @@ public class PostServiceImpl implements PostService {
     // This will make the Url independent.
     String POST = "/posts";
     String POST_BY_ID = "/posts/";
+
 
     private final RestTemplate restTemplate;
 
